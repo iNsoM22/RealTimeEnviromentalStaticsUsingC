273fa1ca14a13d92b11ccbf2253d7c3fd2f63ec5
abdullahathar821 <148074854+abdullahathar821@users.noreply.github.com>
2024-01-13 13:39:01 +0000

    Updated alert.c
    Resolve the problem of Email Headers, Subjectt and Body not showing up in the emails.

--- alert.c
+++ alert.c
@@ -28,12 +28,12 @@ void send_email(const char *from_email, const char *from_password, const char *t
         snprintf(subject_header, 100, "Subject: Alert: Weather Abnormality Report");
         headers = curl_slist_append(headers, subject_header);
         headers = curl_slist_append(headers, ""); // Empty line to separate headers from body
-        headers = curl_slist_append(headers, "This is an automated alert. Attached below is the Abnormality report containing the information which caused this alert to be generated.");
 
         // Initialize the email body
         curl_easy_setopt(curl, CURLOPT_URL, url);
         curl_easy_setopt(curl, CURLOPT_MAIL_FROM, from_email);
         curl_easy_setopt(curl, CURLOPT_MAIL_RCPT, recipients);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers); // Set the headers
         curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
         curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L);
 
@@ -53,6 +53,10 @@ void send_email(const char *from_email, const char *from_password, const char *t
         curl_mime_name(part, "attachment");
         curl_mime_filedata(part, pdf_file_path);
 
+        // Body text
+        part = curl_mime_addpart(mime);
+        curl_mime_data(part, "This is an automated alert. Attached below is the Weather Abnormality Report containing the daily forecast information as well as anomallies, indicating the data that caused them to be generated.", CURL_ZERO_TERMINATED);
+
         // Set the MIME data
         curl_easy_setopt(curl, CURLOPT_MIMEPOST, mime);
 
@@ -65,6 +69,7 @@ void send_email(const char *from_email, const char *from_password, const char *t
 
         // Cleanup
         curl_slist_free_all(recipients);
+        curl_slist_free_all(headers); // Free the headers
         curl_easy_cleanup(curl);
         curl_global_cleanup();
     }
